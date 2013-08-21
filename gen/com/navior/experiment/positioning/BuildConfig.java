104f40ba5325459ccefc6350842b9c6e129400a5
horzwxy <wangxiayang.fdt@gmail.com>
2013-08-21 10:33:48 +0800

    first commit

---
+++ gen/com/navior/experiment/positioning/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.navior.experiment.positioning;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
