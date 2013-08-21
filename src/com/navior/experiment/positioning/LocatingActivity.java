c79543ee592da9ad8e25a2be7366720bb67e6c92
horzwxy <wangxiayang.fdt@gmail.com>
2013-08-21 13:39:39 +0800

    add new activity

---
+++ src/com/navior/experiment/positioning/LocatingActivity.java
@@ -0,0 +1,25 @@
+/**
+ * ==============================BEGIN_COPYRIGHT===============================
+ * ===================NAVIOR CO.,LTD. PROPRIETARY INFORMATION==================
+ * This software is supplied under the terms of a license agreement or
+ * nondisclosure agreement with NAVIOR CO.,LTD. and may not be copied or
+ * disclosed except in accordance with the terms of that agreement.
+ * ==========Copyright (c) 2003 NAVIOR CO.,LTD. All Rights Reserved.===========
+ * ===============================END_COPYRIGHT================================
+ *
+ * @author wangxiayang
+ * @date 21/08/13
+ */
+package com.navior.experiment.positioning;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocatingActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+  }
+}
--- src/com/navior/experiment/positioning/SettingActivity.java
+++ src/com/navior/experiment/positioning/SettingActivity.java
@@ -1,15 +1,37 @@
 package com.navior.experiment.positioning;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.InputType;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+import java.util.HashMap;
 
 public class SettingActivity extends Activity {
+
+  private HashMap<String, LinearLayout> layoutMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
+
+    for( int i = 21; i < 33; i++ ) {
+      SettingLine line = new SettingLine( this, "8765432" + i );
+      ((ViewGroup) findViewById( R.id.root ) ).addView( line );
+    }
+
+    Button button = (Button) findViewById(R.id.save);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+
+      }
+    });
 	}
 
 	@Override
@@ -19,4 +41,94 @@ public class SettingActivity extends Activity {
 		return true;
 	}
 
+  class SettingLine extends LinearLayout {
+    final private CheckBox checkBox;
+    private TextView textView;
+    private EditText x;
+    private EditText y;
+    final private String starname;
+
+    public SettingLine(Context context, final String name) {
+      super(context);
+
+      this.starname = name;
+
+      setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+      setOrientation(HORIZONTAL);
+
+      checkBox = new CheckBox(context);
+      checkBox.setOnClickListener( new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          if(checkBox.isChecked()) {
+            layoutMap.put(starname, SettingLine.this);
+            x.setEnabled(true);
+            y.setEnabled(true);
+          }
+          else {
+            layoutMap.remove(starname);
+            x.setEnabled(false);
+            y.setEnabled(false);
+          }
+        }
+      });
+      addView(checkBox);
+      textView = new TextView(context);
+      textView.setText(name);
+      addView(textView);
+      x = new EditText(context);
+      x.setHint("x\t\t\t\t\t");
+      x.setInputType(InputType.TYPE_CLASS_NUMBER);
+      addView(x);
+      y = new EditText(context);
+      y.setHint("y\t\t\t\t\t");
+      y.setInputType(InputType.TYPE_CLASS_NUMBER);
+      addView(y);
+    }
+
+    public CheckBox getCheckBox() {
+      return checkBox;
+    }
+
+    public int getInputX() {
+      int result = -1;
+      try {
+        result = Integer.parseInt( x.getEditableText().toString() );
+      } catch (NumberFormatException e ) {
+
+      }
+      return result;
+    }
+
+    public int getInputY() {
+      int result = -1;
+      try {
+        result = Integer.parseInt( y.getEditableText().toString() );
+      } catch (NumberFormatException e ) {
+
+      }
+      return result;
+    }
+  }
+
+  class Location {
+    private int x;
+    private int y;
+
+    int getX() {
+      return x;
+    }
+
+    void setX(int x) {
+      this.x = x;
+    }
+
+    int getY() {
+      return y;
+    }
+
+    void setY(int y) {
+      this.y = y;
+    }
+  }
 }
